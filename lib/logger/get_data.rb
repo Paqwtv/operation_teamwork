9af8b3bb80d393140df8a846ab7b79ffc4e2877c
Paqwtv <paqwtv@gmail.com>
2017-08-23 20:55:58 +0300

    added get_data.rb

---
+++ lib/logger/get_data.rb
@@ -0,0 +1,12 @@
+module GetData
+	def get_data
+		class_name = self.class.name
+		puts %w(----------)
+		puts "Class name is #{class_name}"
+		puts "Log time at #{Time.now}"		
+		puts "Object data is #{@attributes.to_h}" unless @attributes.blank? #for model, data from DB
+		puts @_url_options if @_url_options != nil  #for controllers, incoming params frod DSL		
+		puts @_action_name unless @_action_name.blank? #action name for controllers, wich called
+		puts %w(----------)
+	end
+end
